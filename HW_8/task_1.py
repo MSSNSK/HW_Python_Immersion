b26504e3333610ff5da062de07c6d441820f9059
MSSNSK <msw75i38@gmail.com>
2023-07-14 09:11:30 +0700

    All tasks from HW #8 was completed

---
+++ HW_8/task_1.py
@@ -0,0 +1,22 @@
+# Напишите функцию, которая преобразует pickle файл хранящий список словарей в табличный csv файл.
+# Для тестирования возьмите pickle версию файла из предыдущей задачи.
+# Функция должна извлекать ключи словаря для заголовков столбца из переданного файла.
+
+
+import csv
+import pickle
+
+
+def pickle_to_csv(pickle_path, result_path):
+    with (
+        open(pickle_path, 'rb') as p,
+        open(result_path, 'w', encoding='utf-8', newline='') as c
+    ):
+        pkl = pickle.load(p)
+        headers = pkl[0].keys()
+        csv_writer = csv.DictWriter(c, fieldnames=headers)
+        csv_writer.writeheader()
+        csv_writer.writerows(pkl)
+
+
+pickle_to_csv('files/t1_file.pickle', 'files/t1_result_file.csv')
